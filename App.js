f694d07c48b8c81e97f1cd30ba4887a6247e6ef3
Andrea Leal <a.lealhernandez91@gmail.com>
2022-02-11 16:40:02 -0600

    master

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux"; 
+import * as Font from "expo-font";
+import  AppLoading   from "expo-app-loading";
+
+import productsReducer from "./store/reducers/products";
+import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/order";
+import ShopNavigator from "./navigation/ShopNavigator";
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
+});
+
+const store = createStore(rootReducer);
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+};
+
+  
 
 export default function App() {
+   const [fontLoaded, setFontLoaded] = useState(false);
+
+   if(!fontLoaded) {
+    return (
+      <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setFontLoaded(true)}
+      onError={console.warn}
+    />
+  );
+   }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
   );
 }
 
